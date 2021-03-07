592f95e3c5961276979d98b175c49d1e8fa41257
Curso Laravel 6 <victordantas65@yahoo.com>
2021-03-07 12:10:38 -0300

    Segundo commit

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Permite todos os tipos acessos de verbo http (Usar com caltela por conta dos riscos).
+Route::any('/any', function(){
+    return 'Any';
+});
+
+// Você define os metodos aceitos, nesse caso botei apenas GET ou POST.
+Route::match(['get','post'],'/match', function(){
+    return 'Match';
+});
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Rotas passando apenas uma String 
+Route::get('/contato', function(){
+    return 'Contato';
+});
+
+// Rotas pegando as views dentro de resources -> views
+Route::get('/empresa', function(){
+    return view('contact');
+});
+
+// Rotas por post -> Explicação próximas aulas
+Route::post('/register', function(){
+    return '';
 });
